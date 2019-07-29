91ce81fa6f686452e68e1c05af4e3295f102cc7a
hbhatia2011 <hdamani@gmail.com>
2019-07-29 22:18:42 +0100

    git remote add origin https://github.com/hbhatia2011/Personaldetails.git git push -u origin master

---
+++ src/PersonalDetails.java
@@ -0,0 +1,17 @@
+public class PersonalDetails {
+
+    public static void main(String[]args){
+        String name = "Marry Jane";
+        int age = 25;
+        String address = "Flat 1, cherry lane, London, UB0 1TT";
+        long mobile = 7809831310L;
+        float salary = 980.56F;
+
+        System.out.println("Mrs "+name);
+        System.out.println(age+" years");
+        System.out.println(address);
+        System.out.println(mobile);
+        System.out.println("£" +salary+"/-"+" monthly");
+
+    }
+}
